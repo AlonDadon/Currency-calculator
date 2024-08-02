44862547e1f3d995790577d05b3370d53db16bbb
ada1milion <77745921+ada1milion@users.noreply.github.com>
2024-08-02 14:32:30 +0300

    first commit

---
+++ js/service/storage-service.js
@@ -0,0 +1,16 @@
+'use strict'
+console.log('storage-service ready');
+
+const storageService = {
+    loadFromStorage,
+    saveToStorage
+}
+
+function saveToStorage(key, val) {
+    localStorage.setItem(key, JSON.stringify(val))
+}
+
+function loadFromStorage(key) {
+    var val = localStorage.getItem(key)
+    return JSON.parse(val)
+}
