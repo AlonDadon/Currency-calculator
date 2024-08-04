ea18dbd3d57e0a3dc3cfd45355713435f46a647e
ada1milion <77745921+ada1milion@users.noreply.github.com>
2024-08-04 21:44:26 +0300

    The function dynamically renders the types of currencies in the select, as well as the inputs,And I added a transition to the input, to create a more pleasant UX

--- js/controller.js
+++ js/controller.js
@@ -2,6 +2,7 @@
 
 
 function onInit() {
+    renderInput()
     document.querySelector('input[name=count-currency]').addEventListener('input', debounce(onValueCurrency, 1000))
     document.querySelector('select[name=curr-currency-type]').addEventListener('change', onValueCurrency)
 }
@@ -20,6 +21,36 @@ function onValueCurrency() {
     // todo update the dom , create render function
     console.log('valueCurrency', valueCurrency);
 
+}
+
+
+function renderInput() {
+    const inputNames = ['curr-currency', 'curr-currency-1']
+    const currencyType = getCurrencyType()
+    let strHtml = ``
+
+    inputNames.forEach((inputName, idx) => {
+        strHtml += `
+     <div class="calculator-container">
+    
+         <select name="${inputName}" placeholder="usd" aria-label="currency-type">`
+
+        for (const currCurrencyType in currencyType) {
+            strHtml += `<option value="${currCurrencyType}">${currCurrencyType}</option> <br> \n`
+        }
+
+        strHtml += `</select>
+                    
+                    <div class="input-border"></div>
+        
+                    <input type="number" value="1" name="${inputName}" autofocus
+                        oninput="onUpdateCoinsAmount(this.value)">
+        </div>`
+
+    })
+
+    const elDiv = document.querySelector(`.calculator`)
+    elDiv.innerHTML = strHtml
 }
 
 
